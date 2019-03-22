7b3f1a9e67f1108f531a36328018556f14177dce
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-03-22 22:17:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Svetlana"
-var height = parseInt("74")
+var height = parseInt("74") +1
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height) +1
+console.log(height)
